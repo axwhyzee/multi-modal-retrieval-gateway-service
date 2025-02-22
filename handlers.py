d92711617d754dfa800b1174708c27a7e79a46a8
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-23 00:58:24 +0800

    add parent_key ot storage.add

--- handlers.py
+++ handlers.py
@@ -9,8 +9,8 @@ from event_core.domain.types import ObjectType
 def handle_add(data: bytes, file_name: str, user: str) -> None:
     ext = file_name.rsplit(".", 1)[1]
     hash = hashlib.sha256(file_name.encode()).hexdigest()
-    obj_path = f"{user}/{hash}.{ext}"
-    add(data, obj_path, ObjectType.DOC)
+    key = f"{user}/{hash}.{ext}"
+    add(data, key, key, ObjectType.DOC)
 
 
 def handle_query_text(user: str, text: str) -> List[str]:
