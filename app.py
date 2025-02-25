7957db3f7dc533997725f0a3ea1f70ae0d21955b
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-25 15:18:40 +0800

    bugfix: parse req args type

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def add():
 def query_text():
     user: str = request.args["user"]
     text: str = request.args["text"]
-    n_cands: int = request.args.get("n_cands", 20)
-    n_rank: int = request.args.get("n_rank", 4)
+    n_cands = int(request.args.get("n_cands", 20))
+    n_rank= int(request.args.get("n_rank", 4))
     return handle_query_text(user, text, n_cands, n_rank)
 
 
